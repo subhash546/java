4c9748a1103a9f733aac7f4f8bbaa7fc09d231d5
subhash546 <78291367+subhash546@users.noreply.github.com>
2021-05-13 14:28:18 +0530

    Add files via upload

---
+++ Name546.java
@@ -0,0 +1,14 @@
+//write a java program to read your name and display it.
+import java.util.*;
+class Name546
+{
+public static void main(String args[])
+{
+System.out.println("Enter your name :");
+Scanner sc=new Scanner(System.in);
+String name=sc.nextLine();
+System.out.println("Welcome "+name);
+}
+}
+
+
