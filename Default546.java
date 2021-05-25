11914e0a78ddcbf107b70e507ed88e60f413c488
SubhashRowtu <78291367+subhash546@users.noreply.github.com>
2021-05-25 14:04:13 +0530

    Add files via upload

---
+++ Default546.java
@@ -0,0 +1,26 @@
+import java.util.*;
+class Default546
+{
+static byte b;
+static  short s;
+static boolean b1;
+static char ch;
+static long l;
+static int i;
+static float f;
+static double d;
+public static void main (String args[])
+{
+System.out.println("default values of byte is "+b);
+System.out.println("default values of short is "+s);
+System.out.println("default values of boolean is "+b1);
+System.out.println("default values of char is "+ch);
+System.out.println("default values of long is "+l);
+System.out.println("default values of int is "+i);
+System.out.println("default values of float is "+f);
+System.out.println("default values of double is "+d);
+}
+}
+
+
+
